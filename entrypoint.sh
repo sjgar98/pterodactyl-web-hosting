936c67bf484be2fc1ff5c10047b6d2533474d2da
Santiago Garcia <54064646+sjgar98@users.noreply.github.com>
2026-06-10 13:21:58 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,34 +3,15 @@ sleep 1
 
 cd /home/container
 
-# Copy defaults if /home/container/apache2/httpd.conf is empty we copy over the default configs
-if [ ! -d /home/container/apache2 ]; then
-    echo "[Entrypoint] Copying default files..."
-    cp -r /usr/local/default_files/apache2 /home/container
-fi
-
-if [ ! -d /home/container/php-fpm ]; then
-    echo "[Entrypoint] Copying default files..."
-    cp -r /usr/local/default_files/php-fpm /home/container
+if [ ! -f /home/container/apache2.conf ]; then
+    echo "[Entrypoint] Copying default Apache config"
+    cp /etc/apache2/apache2.conf /home/container
 fi
 
 if [ ! -d /home/container/public_html ]; then
-    echo "[Entrypoint] Copying default files..."
+    echo "[Entrypoint] Copying default web root"
     cp -r /usr/local/default_files/public_html /home/container
 fi
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
-# Run the Server
-${MODIFIED_STARTUP}
-
-echo "Starting Redis..."
-redis-server --daemonize yes
-
-echo "Starting PHP-FPM..."
-/usr/sbin/php-fpm83 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
-
 echo "Starting Apache..."
-exec httpd -f /home/container/apache2/httpd.conf -DFOREGROUND
+exec apache2-foreground -f /home/container/apache2.conf
